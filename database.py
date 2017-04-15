ab0096aeb914c16fbbdbffb1c6ebd777a7328615
Sam Messina <samuel.messina@gmail.com>
2017-04-15 10:10:59 -0500

    Clean up getAllServices func to better handle empty arrays

--- database.py
+++ database.py
@@ -56,8 +56,10 @@ def getAllServices():
     see the implementation of our list function in order to do so.
     '''
 
-    serviceArray = collection.find_one({"name": userName})['data']
+    serviceArray = collection.find_one({"name": userName})
+    serviceArray = serviceArray['data'] if serviceArray else serviceArray
     if serviceArray: return serviceArray
+    else: return False
 
 def checkIfServiceExists(name):
     '''
@@ -130,9 +132,8 @@ def getServiceByName(name):
     '''
     Returns a given service for the current user.
     '''
+    serviceArray = getAllServices()
 
-    service = {}
-    serviceArray = collection.find_one({"name": userName})['data']
     for serviceDict in serviceArray:
         if serviceDict["service"] == name:
             service = serviceDict
