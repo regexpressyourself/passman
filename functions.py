6c479ae1d9cfcbbbb636de48edb97c14bfa82e10
Sam Messina <samuel.messina@gmail.com>
2017-04-15 10:11:36 -0500

    Refactor of listServicesPrompt function

--- functions.py
+++ functions.py
@@ -96,18 +96,31 @@ def generatePasswordPrompt():
     print("todo")
 
 def listServicesPrompt():
-    for service in getAllServices():
-        if service['serviceUserName'] == "":
-            if service['serviceUrl'] == "":
-                print(service['service'])
+
+    print('{:20}{:20}'.format('Service/URL', 'Username'))
+    print('-------------------------------------')
+    serviceArray = getAllServices()
+    if not serviceArray:
+        serviceArray = []
+        print("No services to show!\n")
+
+    for service in serviceArray:
+        serviceUserName = service['serviceUserName']
+        serviceUrl = service['serviceUrl']
+        service = service['service']
+
+        if serviceUserName == "":
+            if serviceUrl == "":
+                print(service+'\n')
             else:
-                print(service['service'],service['serviceUrl'],sep='\t')
+                print('{:20}\n{:20}\n'.format(service,serviceUrl))
         else:
-            if service['serviceUrl'] == "":
-                print(service['service'],"-",service['serviceUserName'],sep='\t')
+            if serviceUrl == "":
+                print('{:20}{:20}\n'.format(service,serviceUserName))
             else:
-                print(service['service'],service['serviceUserName'],service['serviceUrl'],sep='\t')
+                print('{:20}{:20}\n{:20}\n'.format(service,serviceUserName,serviceUrl))
     return True
+
 def addServicePrompt(name="",usname="",url=""):
     if not usname == "":
         if checkIfServiceExists(name):
