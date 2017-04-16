9dd09be3ba58fc28573adf8599c8640206402967
Sam Messina <samuel.messina@gmail.com>
2017-04-16 10:21:24 -0500

    moved menu functions to menu.py

--- functions.py
+++ functions.py
@@ -84,12 +84,6 @@ def handleLogin():
         print("Please enter a valid option\n")
         handleLogin()
 
-def welcomeMessage():
-    print("\n\n")
-    print("##################################################")
-    print("# Welcome to Passman!")
-    print("##################################################\n\n")
-
 def generatePasswordPrompt():
     #TODO
     print("todo")
@@ -215,31 +209,3 @@ def isOption(arg):
     else:
         return False
 
-def showMenu():
-    prompt = "What do you want to do?\n\n" \
-    + "(1) List all services\n" \
-    + "(2) Add a new service\n" \
-    + "(3) Remove an existing service\n" \
-    + "(4) Edit an existing service\n" \
-    + "(5) Get password for a service\n" \
-    + "(6) Get username for a service\n" \
-    + "(7) get URL for a service\n\n"
-
-    option = getUserInput(prompt)
-    if option =="1":
-        listServicesPrompt()
-    elif option =="2":
-        addServicePrompt()
-    elif option =="3":
-        removeServicePrompt()
-    elif option =="4":
-        editServicePrompt()
-    elif option =="5":
-        getPassPrompt()
-    elif option =="6":
-        getNamePrompt()
-    elif option =="7":
-        getUrlPrompt()
-    else:
-        print("Didn't get that...\n")
-        showMenu()
