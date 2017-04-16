fd031ae3cee2181996343c05ceafbd8757fce6ae
Sam Messina <samuel.messina@gmail.com>
2017-04-16 10:44:21 -0500

    fix missing imports from commandline.py, and fix printUsage function

--- commandline.py
+++ commandline.py
@@ -3,10 +3,12 @@ Handles all things command line - used from passman.py
 '''
 
 from login import handleLogin
-from menu import welcomeMessage
+from menu import welcomeMessage, listServicesPrompt,\
+        addServicePrompt, editServicePrompt, removeServicePrompt,\
+        getUrlPrompt, getNamePrompt
 
-def printUsage():
-    print("Usage:",sys.argv[0],"[{add [service name] [service username] [service url] | remove [service name] | edit [service name]| list | pass [service name] | uname [service name] | url [service name]}]")
+def printUsage(argv):
+    print("Usage:",argv[0],"[{add [service name] [service username] [service url] | remove [service name] | edit [service name]| list | pass [service name] | uname [service name] | url [service name]}]")
     quit()
 
 def isOption(arg):
@@ -16,7 +18,7 @@ def isOption(arg):
 
 def handleCLArgs(argv):
     if not isOption(argv[1]):
-        printUsage()
+        printUsage(argv)
 
     welcomeMessage()
     handleLogin()
@@ -37,7 +39,7 @@ def handleCLArgs(argv):
     elif argv[1]=="url":
         handleCLURL(argv)
     else:
-        printUsage()
+        printUsage(argv)
 
 def handleCLAdd(argv):
     if len(argv)==2:
@@ -49,7 +51,7 @@ def handleCLAdd(argv):
     elif len(argv)==5:
         addServicePrompt(argv[2],argv[3],argv[4])
     else:
-        printUsage()
+        printUsage(argv)
 
 def handleCLRemove(argv):
     if len(argv)==2:
@@ -57,7 +59,7 @@ def handleCLRemove(argv):
     elif len(argv)==3:
         removeServicePrompt(argv[2])
     else:
-        printUsage()
+        printUsage(argv)
 
 def handleCLEdit(argv):
     if len(argv)==2:
@@ -65,7 +67,7 @@ def handleCLEdit(argv):
     elif len(argv)==3:
         editServicePrompt(argv[2])
     else:
-        printUsage()
+        printUsage(argv)
 
 def handleCLPass(argv):
     print("TODO")
@@ -76,13 +78,13 @@ def handleCLUName(argv):
     elif len(argv)==3:
         getNamePrompt(argv[2])
     else:
-        printUsage()
+        printUsage(argv)
 
 def handleCLURL(argv):
     if len(argv)==2:
         getUrlPrompt()
     elif len(argv)==3:
-        getUrlPrompt(argv[2])
+        getUrlPrompt(argv[5])
     else:
-        printUsage()
+        printUsage(argv)
 
