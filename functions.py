99d317595e726bcd9175cff883359a1e5161a9d9
nosnixster <nosnixster@gmail.com>
2017-04-17 15:09:54 -0500

    Don't leave stray threads running when won't be used.

--- functions.py
+++ functions.py
@@ -14,6 +14,23 @@ import pyperclip
 # Generic Functions
 ############################################################
 
+myThread = None
+class StoppingThread(Thread):
+    def __init__(self, target, args):
+        super(StoppingThread, self).__init__(target=target,args=args)
+        self._status = 'running'
+    def stop(self):
+        if(self._status=='running'):
+            self._status = 'stopping'
+    def stopped(self):
+      self._status = 'stopped'
+    def is_running(self):
+        return (self._status == 'running')
+    def is_stopping(self):
+        return (self._status == 'stopping')
+    def is_stopped(self):
+        return (self._status == 'stopped')
+
 def quit():
     print("\nSee you later!\n")
     sys.exit()
@@ -38,11 +55,21 @@ def clipboard(text,prnt, clear):
     if prnt:
         print(text)
     if clear:
-        myThread = Thread(target=timer, args=(20,text,))
+        global myThread
+        if myThread and myThread.is_running():
+            myThread.stop()
+            myThread.join()
+        myThread = StoppingThread(target=timer, args=(20,text,))
         myThread.start()
 
 def timer(seconds,text):
-    sleep(seconds)
+    global myThread
+    for i in range(seconds):
+        if myThread.is_running():
+            sleep(1)
+        else:
+            myThread.stopped()
+            return
     clearclip(text)
 
 def clearclip(text):
