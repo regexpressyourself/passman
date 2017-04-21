efc96a0cc77a7d9a761f81617da717aece3f74ce
Sam Messina <samuel.messina@gmail.com>
2017-04-21 09:49:00 -0500

    Check connection early on in passman.py

--- passman.py
+++ passman.py
@@ -8,9 +8,11 @@ import sys
 
 from splash import showSplash
 
-from login import handleLogin
+from login import handleLogin, handleOfflineLogin
 from commandline import handleCLArgs
 from menu import showMenu, welcomeMessage
+from database import checkConnection
+from offlinemenu import handleOfflineMenu
 
 def main():
     if len(sys.argv) > 1:
@@ -18,9 +20,16 @@ def main():
 
     else:
         welcomeMessage()
-        handleLogin()
-        while True:
-            showMenu()
+        if checkConnection("check"): 
+            handleLogin()
+            while True:
+                showMenu()
+        else:
+            handleOfflineLogin()
+            while True:
+                showMenu()
+                handleOfflineMenu
+
 
 
 
