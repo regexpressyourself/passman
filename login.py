2ee0d1ed76bbe3801afc92ff2c7eaf92672334fa
Sam Messina <samuel.messina@gmail.com>
2017-04-21 09:48:20 -0500

    Better vetting for offline login - checking password and username

--- login.py
+++ login.py
@@ -63,3 +63,44 @@ def signUpUser():
         print("Sorry, that username is already taken")
         signUpUser()
 
+def getOfflineUsername():
+    username = getUserInput("Please enter your username")
+    file_path = os.path.expanduser("~/.passman/{}.json".format(username))
+    while not os.path.isfile(file_path):
+        print("Sorry, that user is does not have any saved data")
+        username = getUserInput("Please enter your username")
+        file_path = os.path.expanduser("~/.passman/{}.json".format(username))
+    return username
+
+def getOfflinePassword(data):
+    pw = getUserInput("Please enter your password", True)
+    key = hashlib.sha256(pw.encode()).digest()
+    hashedpw = hashlib.sha512(pw.encode('utf-8')).hexdigest()
+
+    while hashedpw != data['password']:
+        print("Wrong password")
+        pw = getUserInput("Please enter your password", True)
+        key = hashlib.sha256(pw.encode()).digest()
+        hashedpw = hashlib.sha512(pw.encode('utf-8')).hexdigest()
+
+def handleOfflineLogin():
+    print("NOTE: No connection")
+    print("Continuing in offline mode. \nYou can retrieve any service data, " \
+            +"but you will not be \nable to edit or upload data\n\n")
+    dir_path = os.path.expanduser("~/.passman")
+    if not os.path.isdir(dir_path):
+        print("Sorry, no local users found")
+        quit()
+
+    username = getOfflineUsername()
+    file_path = os.path.expanduser("~/.passman/{}.json".format(username))
+
+    with open(file_path) as data_file:
+        data = data_file.read()
+    data = ast.literal_eval(data)
+
+    key = getOfflinePassword(data)
+
+    handleOfflineMenu(username, key)
+
+    return True
