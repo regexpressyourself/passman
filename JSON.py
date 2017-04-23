1506e4e95bb2be303ac4a2db97500091529505da
Sam Messina <samuel.messina@gmail.com>
2017-04-23 09:45:10 -0500

    Add function to retrieve a single service from the JSON file

--- JSON.py
+++ JSON.py
@@ -2,6 +2,7 @@ import os
 import json
 import ast
 from functions import quit
+from encryption import encrypt, decrypt
 
 def getServicesOffline(name):
     dir_path = os.path.expanduser("~/.passman")
@@ -10,9 +11,14 @@ def getServicesOffline(name):
             not os.path.isdir(dir_path):
         print("No local file found - exiting")
         quit()
-    with open(file_path) as data_file:    
+    with open(file_path) as data_file:
         data = data_file.read()
-    data = ast.literal_eval(data)
+    data = ast.literal_eval(data)['data']
     return data
 
-
+def getServiceDataOffline(name, sname, key):
+    serviceArray = getServicesOffline(name)
+    for service in serviceArray:
+        if decrypt(service['service'], key) == sname:
+            return service
+    return False
