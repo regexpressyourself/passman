d6d0fb4afb30f19600c8d4d7b064883559cb62f2
Sam Messina <samuel.messina@gmail.com>
2017-05-01 13:20:33 -0500

    Fix setup.py's imports

--- setup.py
+++ setup.py
@@ -1,24 +1,25 @@
-from distutils.core import setup
+from setuptools import setup, find_packages  # Always prefer setuptools over distutils
 
 setup (
         name = 'passman',
-        packages = ['passman'], # this must be the same as the name above
-        scripts = ['passman/passman'],
-        version = '1.0.4',
+        packages =find_packages(exclude=['build', 'docs', 'templates']), # this must be the same as the name above
+        include_package_data=True,
+        py_modules=['commandline', 'database', 'encryption', 'functions', 'JSON', 'login', 'menu', 'offlinemenu'],
+        version = '1.1.5',
         description = 'A terminal-based password manager',
         author = 'Sam Messina',
         author_email = 'samuel.messina@gmail.com',
         url = 'https://github.com/regexpressyourself/passman', # use the URL to the github repo
         download_url = 'https://github.com/regexpressyourself/passman/1.0.tar.gz', # I'll explain this in a second
         keywords = ['password', 'manager', 'terminal'], # arbitrary keywords
-        entry_points={'console_scripts':['passman=passman:start']},
+        entry_points={'console_scripts':['passman=passman.__main__:main']},
         install_requires=[
             'pymongo',
             'asciimatics',
             'pyperclip',
             'Crypto',
+            'pycrypto',
             'argparse'
             ],
-        license='MIT',
-        classifiers = [],
+        license='MIT'
         )
