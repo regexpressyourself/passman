b4dac1ed5aaa6e59d9ada205d2f66ff9b4831c6d
Sam Messina <samuel.messina@gmail.com>
2017-05-02 00:05:58 -0500

    updated setup.py with download link

--- setup.py
+++ setup.py
@@ -6,21 +6,24 @@ def readme():
     with open('README.rst') as f:
         return f.read()
 
-long_description = readme()
+try:
+    long_description = readme()
+except:
+    long_description = 'See https://github.com/regexpressyourself/passman/blob/master/README.md for more'
 
 setup (
         name = 'passman',
-        packages =find_packages(exclude=['build', 'docs', 'templates']), # this must be the same as the name above
+        packages =find_packages(exclude=['build', 'docs', 'templates']),
         include_package_data=True,
         py_modules=['commandline', 'database', 'encryption', 'functions', 'JSON', 'login', 'menu', 'offlinemenu'],
-        version = '1.1.7',
+        version = '1.1.8',
         description = 'A terminal-based password manager',
         long_description = long_description,
         author = 'Sam Messina',
         author_email = 'samuel.messina@gmail.com',
-        url = 'https://github.com/regexpressyourself/passman', # use the URL to the github repo
-        download_url = 'https://github.com/regexpressyourself/passman/1.0.tar.gz', # I'll explain this in a second
-        keywords = ['password', 'manager', 'terminal'], # arbitrary keywords
+        url = 'https://github.com/regexpressyourself/passman',
+        download_url = 'https://github.com/regexpressyourself/passman/1.1.8.tar.gz',
+        keywords = ['password', 'manager', 'terminal'],
         entry_points={'console_scripts':['passman=passman.__main__:main']},
         install_requires=[
             'pymongo',
